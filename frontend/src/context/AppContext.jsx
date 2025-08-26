7a72f223101fa1546cc784dcde20dffe7c4ab283
SatyamGarg297 <satyamgarg1672001@gmail.com>
2025-08-26 23:13:10 +0530

    Created Patient Login and Registration system

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -8,13 +8,9 @@ const AppContextProvider = (props) => {
   
     const currencySymbol = '$'
     const backendUrl = import.meta.env.VITE_BACKEND_URL
+
     const [doctors,setDoctors] = useState([])
-
-
-    const value = {
-        doctors,
-        currencySymbol
-    }
+    const [token,setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):false)
 
     const getDoctorsData = async () => {
 
@@ -32,6 +28,13 @@ const AppContextProvider = (props) => {
             toast.error(error.message)
         }
 
+    }
+
+    const value = {
+        doctors,
+        currencySymbol,
+        token,setToken,
+        backendUrl
     }
 
     useEffect(()=>{
